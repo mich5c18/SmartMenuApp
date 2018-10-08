7aa2a59ec3c4d95c22c4116294abf491fb66aead
michaelserupmj <michaelserupmj@gmail.com>
2018-10-08 13:03:33 +0200

    Add project files.

---
+++ SmartMenuApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+
+namespace SmartMenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            SmartMenu menu = new SmartMenu();
+            Console.WriteLine("For Dansk: Tryk 1");
+            Console.WriteLine("For English: Press 2");
+            string input = Console.ReadLine();
+
+            int.TryParse(input, out int r); // Tester om input fra brugeren er et tal
+            if (r == 0)
+            {
+                Console.WriteLine("Farvel");
+            }
+            else
+            {
+                menu.LoadMenu(menu.ChooseLanguage(r));
+                menu.Activate();
+            }
+        }
+
+    }
+}
